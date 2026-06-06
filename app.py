e97cafb7ea584aa66d2dbfc44858428e02d3529b
Dev9746 <devs72527@gmail.com>
2026-06-06 21:52:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 import streamlit as st
 import joblib
-import traceback
 
-st.set_page_config(page_title="Model Test")
+st.set_page_config(page_title="Test")
 
-st.title("Model Test")
+st.title("Joblib Test")
+st.success("Before loading model")
 
-try:
-    scaler = joblib.load("scaler.pkl")
-    st.success("✅ scaler.pkl loaded")
-except Exception:
-    st.error("❌ scaler.pkl failed")
-    st.code(traceback.format_exc())
-
-try:
-    model = joblib.load("model.pkl")
-    st.success("✅ model.pkl loaded")
-except Exception:
-    st.error("❌ model.pkl failed")
-    st.code(traceback.format_exc())
+# model load mat karo
+st.success("After loading section")
